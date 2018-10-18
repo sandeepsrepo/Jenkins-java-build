6fdcc7baf701c9610db776b6b74df70ae0a70df7
sandeepsrepo <41971526+sandeepsrepo@users.noreply.github.com>
2018-10-18 21:44:58 +0530

    Update BuildTest.java

--- src/com/test/build/BuildTest.java
+++ src/com/test/build/BuildTest.java
@@ -6,7 +6,7 @@ public class BuildTest {
 		
 		for(int i = 0; i < 10; i++) {
 			
-			System.out.println("Checking in the new code and then printing  --  the current iteration : "+i);
+			System.out.println("Hi - Checking in the new code and then printing  --  the current iteration : "+i);
 			
 			try {
 				Thread.sleep(1000);
