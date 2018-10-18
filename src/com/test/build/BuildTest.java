019c46ec65484b27b14899c3abd2e0fc447c8d29
sandeep <sandeep.nandanan@gmail.com>
2018-10-18 21:29:38 +0530

    initial code checkin

---
+++ src/com/test/build/BuildTest.java
@@ -0,0 +1,21 @@
+package com.test.build;
+
+public class BuildTest {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {
+			
+			System.out.println("Changing the code and then printing  --  the current iteration : "+i);
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+
+}
